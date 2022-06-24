faab56b6add439c14b88fba8c094567bb831b9e2
Den <denis100703@gmail.com>
2022-06-24 03:34:30 +0300

    Improve cache hash check

--- collager.py
+++ collager.py
@@ -1,12 +1,21 @@
 from PIL import Image
 from tqdm import tqdm
-import hashlib
 import os
 
+# вычисляет хэш нескольких файлов
+from hashlib import md5
+def files_hash(files):
+  temp = md5()
+  for file in tqdm(files, desc="calculating files hash"):
+    with open(file, "rb") as f:
+      for chunk in iter(lambda: f.read(4096), b""):
+        temp.update(chunk)
+  return temp.hexdigest()
+
 # путь к папке с картинками
-path = "C:\\Users\\Potato\\Desktop\\cats_dataset\\best"
+path = "C:\\Users\\Potato\\Desktop\\cats_dataset"
 cache_file = ".cache_data"
-cache_data = os.path.join(path, cache_file)
+cache_path = os.path.join(path, cache_file)
 
 # штрина и высота
 width, height = (1920, 1080)
@@ -15,13 +24,7 @@ width, height = (1920, 1080)
 lines = 5
 height_per_line = height // lines
 
-
-if os.path.exists(cache_data):
-  with open(cache_data, "r") as file:
-    files_hash = file.readline().strip()
-    cache_data = [line.split(" | ") for line in file.read().splitlines()]
-    cache_data = {file:ratio for file, ratio in cache_data}
-
+# получаем все изображения в папке
 images = [os.path.join(path, file) for file in os.listdir(path)]
 images = [file for file in images
   if os.path.isfile(file)
@@ -30,28 +33,17 @@ images = [file for file in images
   )
 ]
 
-def file_hash(file):
-  temp = hashlib.md5()
-  with open(file, "rb") as f:
-    for chunk in iter(lambda: f.read(4096), b""):
-      temp.update(chunk)
-  return temp.hexdigest()
+# проверяем кеш
+if os.path.exists(cache_path):
+  images_hash = files_hash(images)
+  cache_hash = ""
+  with open(cache_path, "r") as file:
+    cache_hash = file.readline().strip()
+    cache_data = [line.split(" | ") for line in file.read().splitlines()]
+    cache_data = {file:ratio for file, ratio in cache_data}
 
-images_md5 = ""
-for image in tqdm(images):
-  images_md5 += file_hash(image)
-
-images_md5 = hashlib.md5(images_md5.encode("utf-8")).hexdigest()
-print(images_md5)
-
-print(files_hash == images_md5)
-
-# здесь у нас в памяти находятся пути к картинкам в папке
-# далее нужно прочитать файл, содержащий соотношения сторон картинок
-# еще нужно проверить соответствие текущих файлов в папке и считанных из файла
-
-# препроцессинг изображений
-# рассчет соотношения сторон для каждого изображения и сохранение в файл
+if not os.path.exists(cache_path) or cache_hash != images_hash:
+  pass # рассчет соотношения сторон для каждого изображения и сохранение в файл
 
 collage = Image.new("RGBA", (width, height))
 
