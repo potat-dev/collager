f403731214bc192b0815660696ccccb24c1bc22f
Den <denis100703@gmail.com>
2022-06-26 15:32:58 +0300

    Fixed inaccurate rounding and cropping

--- center_crop.py
+++ center_crop.py
@@ -1,19 +1,68 @@
 from PIL import Image
+from random import choice
 
-def center_crop(img, height, crop_ratio):
+# функция для обрезки изображения по центру с заданным соотношением сторон
+def center_crop(img, height, crop_ratio, scale_method):
   crop_ratio = crop_ratio
-  ideal_width = int(height * crop_ratio)
+  ideal_width = round(height * crop_ratio)
   if img.width / img.height > crop_ratio:
     # crop the left and right edges:
-    offset = (img.width - crop_ratio * img.height) / 2
+    offset = round((img.width - crop_ratio * img.height) / 2)
     resize = (offset, 0, img.width - offset, img.height)
   else:
     # crop the top and bottom edges:
-    offset = (img.height - img.width // crop_ratio) / 2
+    offset = round((img.height - img.width / crop_ratio) / 2)
     resize = (0, offset, img.width, img.height - offset)
-  return img.crop(resize).resize((ideal_width, height), Image.BICUBIC)
+  return img.crop(resize).resize((ideal_width, height), scale_method)
 
-image = Image.open('cat.jpg')
-image = center_crop(image, 200, 1.25)
-image.save('cat_crop.jpg')
-print(image.size, "aspect ratio:", image.width / image.height)
+# функция для создания одной линии коллажа
+def create_line(images, width, line_height, ratio_delta=0.01, scale_method=Image.LANCZOS):
+  def sum_ratios(items):
+    return sum([item["ratio"] for item in items])
+
+  height_shift = ratio_delta * line_height
+  line_ratio = width / line_height
+  min_ratio = width / (line_height + height_shift)
+  max_ratio = width / (line_height - height_shift)
+
+  iters = 0
+  selected_ratios = []
+  while selected_ratios == []:
+    iters += 1
+    while sum_ratios(selected_ratios) < min_ratio:
+      selected_ratios.append(choice(images))
+      # TODO: не допускать повторы изображений
+      # 3 уровня настройки повторов
+      # 1) разрешить любые повторы
+      # 2) запретить повторы в одной линии
+      # 3) запретить повторы во всем коллаже
+    if sum_ratios(selected_ratios) > max_ratio:
+      selected_ratios = []
+
+  curr_ratio = sum_ratios(selected_ratios)
+  ratio_delta = line_ratio - curr_ratio
+
+  old_ratios = [item["ratio"] for item in selected_ratios]
+  new_ratios = [
+    item["ratio"] + ratio_delta * item["ratio"] / curr_ratio
+    for item in selected_ratios
+  ]
+
+  # создаем изображение с размерами width и line_height
+  line = Image.new("RGB", (width, line_height))
+  current_x = 0
+
+  # применяем новые параметры к изображениям
+  for i, image in enumerate(selected_ratios):
+    img = Image.open(image["path"])
+    img = center_crop(img, line_height, new_ratios[i], scale_method)
+    # append img to line
+    line.paste(img, (current_x, 0))
+    # TODO: сначала сохранять изображения в массив
+    # потом считать их общую ширину и создавать линию данной ширины
+    # а потом растягивать линию до нужного размера
+    # тогда не будет черных полос в конце
+    current_x += img.width
+    img.close()
+  
+  return line.resize((width, line_height), scale_method), iters
--- collager.py
+++ collager.py
@@ -1,12 +1,23 @@
 from PIL import Image
 from tqdm import tqdm
-from random import choice
+from center_crop import center_crop, create_line
 import os, logging
 
-from center_crop import center_crop
+# settings
+
+path = "C:\\Users\\Potato\\Desktop\\cats_dataset\\best"
+width, height = (1920, 1080)
+lines = 5
+ratio_delta = 0.01
+scale_method = Image.LANCZOS
 
 log_level = logging.DEBUG
 log_format = '[%(levelname)s]: %(message)s'
+
+# set up logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(log_level)
 
 class TqdmLoggingHandler(logging.Handler):
   def __init__(self, level=logging.NOTSET):
@@ -20,26 +31,14 @@ class TqdmLoggingHandler(logging.Handler):
     except Exception:
       self.handleError(record)
 
-logger = logging.getLogger(__name__)
 handler = TqdmLoggingHandler()
-
-logger.setLevel(log_level)
 handler.setLevel(log_level)
 
 handler.setFormatter(logging.Formatter(log_format))
 logger.addHandler(handler)
 
-# путь к папке с картинками
-path = "C:\\Users\\Potato\\Desktop\\cats_dataset\\best"
+# get all images in folder
 
-# штрина и высота
-width, height = (1920, 1080)
-
-# количество строк в коллаже
-lines = 5
-line_height = height // lines
-
-# получаем все изображения в папке
 images = [os.path.join(path, file) for file in os.listdir(path)]
 images = [file for file in images
   if os.path.isfile(file)
@@ -48,21 +47,15 @@ images = [file for file in images
   )
 ]
 
-# рассчет соотношения сторон для каждого изображения
+# calculate aspect ratio for each image
+
 logger.debug(f"images in folder: {len(images)}")
 
 cache_data = []
 for image in tqdm(images[:], desc="calculating ratios"):
   try:
     img = Image.open(image)
-    cache_data.append(
-      {
-        "path": image,
-        "width": img.width,
-        "height": img.height,
-        "ratio": img.width / img.height
-      }
-    )
+    cache_data.append({"path": image, "ratio": img.width / img.height})
     img.close()
   except:
     logger.warning(f"unable to open the image: {os.path.basename(image)}")
@@ -70,63 +63,14 @@ for image in tqdm(images[:], desc="calculating ratios"):
 
 logger.debug(f"valid images: {len(images)}")
 
-# функция для создания одной линии коллажа
-def create_line(images, width, line_height, ratio_delta=0.01):
-  def sum_ratios(items):
-    return sum([item["ratio"] for item in items])
-
-  height_shift = ratio_delta * line_height
-  line_ratio = width / line_height
-  min_ratio = width / (line_height + height_shift)
-  max_ratio = width / (line_height - height_shift)
-
-  iters = 0
-  selected_ratios = []
-  while selected_ratios == []:
-    iters += 1
-    while sum_ratios(selected_ratios) < min_ratio:
-      selected_ratios.append(choice(images))
-    if sum_ratios(selected_ratios) > max_ratio:
-      selected_ratios = []
-  
-  # log iters
-  logger.debug(f"iters: {iters}")
-
-  curr_ratio = sum_ratios(selected_ratios)
-  ratio_delta = line_ratio - curr_ratio
-
-  old_ratios = [item["ratio"] for item in selected_ratios]
-  new_ratios = [
-    item["ratio"] + ratio_delta * item["ratio"] / curr_ratio
-    for item in selected_ratios
-  ]
-
-  # создаем изображение с размерами width и line_height
-  line = Image.new("RGB", (width, line_height))
-  current_x = 0
-
-  # применяем новые параметры к изображениям
-  for i, image in enumerate(selected_ratios):
-    img = Image.open(image["path"])
-    img = center_crop(img, line_height, new_ratios[i])
-    # append img to line
-    line.paste(img, (current_x, 0))
-    current_x += img.width
-    img.close()
-  
-  return line.resize((width, line_height), Image.LANCZOS)
-
 # create collage
+
 collage = Image.new("RGBA", (width, height))
+line_height = height // lines
+
 for line_n in range(lines):
-  line = create_line(cache_data, width, line_height)
+  line, iters = create_line(cache_data, width, line_height, ratio_delta, scale_method)
+  logger.debug(f"line {line_n} created, {iters} iterations")
   collage.paste(line, (0, line_n * line_height))
-  pass # конструировать строку картинок и добавлять ее на коллаж
 
-# save collage
 collage.save("collage.png")
-
-# dump cache_data to file with json for debug
-# import json
-# with open("cache_data.json", "w") as f:
-#   json.dump(cache_data, f)
