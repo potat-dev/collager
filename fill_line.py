e1f4bae335c5bda5c07aaf9eef0a6506a4362899
Den <denis100703@gmail.com>
2022-06-26 13:17:03 +0300

    Proportional image cropping implemented

--- fill_line.py
+++ fill_line.py
@@ -3,34 +3,30 @@ import json
 with open("cache_data.json", "r") as f:
     cache_data = json.load(f)
 
-# select all ratios from cache_data
-ratios = [item["ratio"] for item in cache_data.values()]
+cache_data = [{"path": path, **v} for path, v in cache_data.items()]
 
-# print average of the ratios list
-print(sum(ratios) / len(ratios))
+# function to sum up all ratios in list of items from cache_data
+def sum_ratios(items):
+    return sum([item["ratio"] for item in items])
+
+# select all ratios from cache_data
+ratios = [item["ratio"] for item in cache_data]
 
 height = 1080
 width = 1920
 lines = 4
-height_shift = 0.01
+height_shift = 0.001
 
 line_height = height / lines
 height_shift = height_shift * line_height
 min_height = line_height - height_shift
 max_height = line_height + height_shift
 
+line_ratio = width / line_height
 min_ratio = width / max_height
 max_ratio = width / min_height
 
 print("min_ratio:", min_ratio, "max_ratio:", max_ratio)
-
-# print min and max height
-print("min", min_height, "max", max_height)
-
-min_items = max_ratio / max(ratios)
-max_items = min_ratio / min(ratios)
-
-print("min_items:", min_items, "max_items:", max_items)
 
 import random
 
@@ -38,10 +34,28 @@ iters = 0
 selected_ratios = []
 while selected_ratios == []:
     iters += 1
-    while sum(selected_ratios) < min_ratio:
+    while sum_ratios(selected_ratios) < min_ratio:
         # append a random ratio to the selected_ratios list
-        selected_ratios.append(random.choice(ratios))
-    if sum(selected_ratios) > max_ratio:
+        selected_ratios.append(random.choice(cache_data))
+    if sum_ratios(selected_ratios) > max_ratio:
         selected_ratios = []
 
-print(selected_ratios, sum(selected_ratios), iters)
+# import pprint
+# pprint.pprint(selected_ratios)
+print("found after", iters, "iterations")
+
+# TODO: write code to crop + resize images to fit line height and width
+# вычислить пропорциональное изменение соотношения сторон для каждого изображения
+# для маленьких изображений изменение будет меньше, для больших изображений изменение будет больше
+
+curr_ratio = sum_ratios(selected_ratios)
+ratio_delta = line_ratio - curr_ratio
+
+old_ratios = [item["ratio"] for item in selected_ratios]
+new_ratios = [item["ratio"] + ratio_delta * item["ratio"] / curr_ratio for item in selected_ratios]
+
+print("line ratio:", line_ratio)
+print("old_ratios:", old_ratios)
+print("sum:", sum(old_ratios))
+print("new_ratios:", new_ratios)
+print("sum:", sum(new_ratios))
